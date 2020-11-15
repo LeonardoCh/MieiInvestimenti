4228409d5eee4dd050a4126066865c453dab3567
LeonardoCh <leonardo.chiodi@hotmail.it>
2020-11-15 14:31:29 +0000

    Secondo commit

--- OrdinaTitoli.scala
+++ OrdinaTitoli.scala
@@ -32,30 +32,6 @@ val SAV : RDD[String] = SimboliAscendenti.map(item => item._1.distinct);
 val SACOUNT : Int = SAV.distinct.count.toInt;
 
 val SAG1ACOUNT : Int = SAG1A(0).size;
-                        
-
-/*
- *
- * Vorrei ottenere, tramite un ciclo for, una serie di variabili.
- * Ognuna di queste variabili deve avere un nome diveros.
- *
- * Questo nome deve essere composto, nella sua prima parte in tutte
- * uguale: SAGA, piu una seconda parte variabile, che corrisponde al
- * nome dello stesso titolo in analisi.
- *
- * Ad esempio per il titolo "BG", avrò una variabile chiamata 
- * "SAGABG", allo stesso modo, per il titolo "AMP", avrò: "SAGAAMP",
- * e cosi via.
- *
- * Ognuna di queste variabili, prese separatamente, deve essere del
- * medesimo tipo di quella che, in questo codice è la variabile 
- * "VettoreElementi", oppure "VettoreElementiRDD".
- *
- * Dovrà cioè essere una distinta variabile, di tipo "Vector", ivi 
- * contenete tutti gli elementi associati a quello specifico 
- * titolo.
- *
-*/
 
 /*
  *
